e6fafc53943e9761459efe623f02c46ebc8968ef
Eddy Cizeron <eddy.cizeron@gmail.com>
2025-03-16 21:21:31 +0100

    Improved documentation

--- README.md
+++ README.md
@@ -2,10 +2,20 @@
 
 The [builder pattern](https://en.wikipedia.org/wiki/Builder_pattern) is encouraged in the Rust language, in particular as a strategy to emulate named and optional arguments, which are intentionally not supported by Rust. However creating all the builder machinery can lead to boilerplate code, in particular when the generated data is complex and multi-layered. The usual builder pattern is based on mutation and generally turns compile-time checks that the final object is complete to a runtime verification. Assemblist allows you to create immutable builders structured as method chains like in
 ```rust
-fn define_movie(name: String)
+fn define_movie<'a>(name: &'a str)
     .released_in(release_year: usize)
-    .directed_by(director_name: String) -> Movie
+    .directed_by(director_name: &'a str) -> Movie
 {
-    Movie { name, release_year, director_name }
+    Movie {
+        name: name.to_string(),
+        release_year,
+        director_name: director_name.to_string(),
+    }
 }
+```
+You can then just call it as it was declared
+```rust
+let movie = define_movie("The Lobster")
+    .released_in(2015)
+    .directed_by("Yorgos Lanthimos");
 ```
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! Macros to easily create immutable builders.
+//! A macro to easily create immutable builders.
 //!
 //! The [builder pattern](https://en.wikipedia.org/wiki/Builder_pattern) is encouraged
 //! in the Rust language, in particular as a strategy to emulate named and optional arguments,
@@ -9,12 +9,22 @@
 //! the final object is complete to a runtime verification. Assemblist allows you to create immutable builders
 //! structured as method chains like in
 //! ```ignore
-//! fn define_movie(name: String)
+//! fn define_movie<'a>(name: &'a str)
 //!     .released_in(release_year: usize)
-//!     .directed_by(director_name: String) -> Movie
+//!     .directed_by(director_name: &'a str) -> Movie
 //! {
-//!     Movie { name, release_year, director_name }
+//!    Movie {
+//!        name: name.to_string(),
+//!        release_year,
+//!        director_name: director_name.to_string(),
+//!    }
 //! }
+//! ```
+//! You can then just call it as it was declared
+//! ```ignore
+//! let movie = define_movie("The Lobster")
+//!     .released_in(2015)
+//!     .directed_by("Yorgos Lanthimos");
 //! ```
 
 use model::tree::Tree;
@@ -25,32 +35,42 @@ mod model;
 mod tools;
 
 /**
- * A macro used to generate immutable builders for functions and methods.
+ * The point of this crate. Generate immutable builders based on chains of methods.
  *
- * The argument of the assemblist! macro is a scope containing either method chains or implementations containing
- * method chains. A method chain looks like a function where the name and argument list are split into multiple parts.
- * Behind the scene assemblist actually creates as many disctinct methods and generates their result type automatically.
+ * The argument of the `assemblist!` macro is a scope containing one or more method chains.
+ * A method chain is similar to a function except that its name and argument list are split into multiple sections.
+ * Behind the scene, `assemblist!` actually creates as many distinct methods and generates their respective result
+ * type automatically.
  * ```ignore
- * fn define_movie(name: String)
- *     .released_in(release_year: usize)
- *     .directed_by(director_name: String) -> Movie
- * {
- *   Movie { name, release_year, director_name }
+ * assemblist! {
+ *     fn define_movie<'a>(name: &'a str)
+ *         .released_in(release_year: usize)
+ *         .directed_by(director_name: &'a str) -> Movie
+ *     {
+ *         Movie {
+ *             name: name.to_string(),
+ *             release_year,
+ *             director_name: director_name.to_string(),
+ *         }
+ *     }
  * }
  * ```
- * Here is a similar chain method declared inside an impl scope:
+ * # For building objects ... among others
+ * 
+ * Note that you can use `assemblist!` with valid rust code for the body content. It does not have to
+ * be a constructor like in the previous example.
  * ```ignore
- * impl MovieMaker {
- *   fn define_movie(name: String)
- *       .released_in(release_year: usize)
- *       .directed_by(director_name: String) -> Movie
- *   {
- *     Movie { name, release_year, director_name }
- *   }
+ * assemblist! {
+ *     pub fn replace_in<'a>(string: &'a str)
+ *         .occurrences_of(pattern: &'a str)
+ *         .with(to: &'a str)
+ *         .at_most(n: usize)
+ *         .times() -> String
+ *     {
+ *         string.replacen(pattern, to, n)
+ *     }
  * }
  * ```
- * Note that the latter pattern is only valid for inherent implementations.
- * You cannot add method chains to trait implementations.
  */
 #[proc_macro]
 pub fn assemblist(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
